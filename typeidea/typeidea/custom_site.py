a517fc46040ab013f5b5ef4cb3c9283aeba7537a
chenlin <907169968@qq.com>
2019-04-28 18:37:54 +0800

    add custom_site

---
+++ typeidea/typeidea/custom_site.py
@@ -0,0 +1,8 @@
+from django.contrib.admin import AdminSite
+
+class CustomSite(AdminSite):
+    site_header = 'Tytpeidea'
+    site_title = 'Typeidea 管理后台'
+    index_title = '首页'
+
+custom_site = CustomSite(name='cus_admin')
