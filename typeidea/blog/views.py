8a6c5f5c3d4d8d145573f9365bec5d0e3fb8e621
chenlin <907169968@qq.com>
2019-05-07 17:27:23 +0800

    add template

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def post_list(request, category_id=None, tag_id=None):
+    return render(request, 'blog/list.html', context = {'name': 'post_list'})
+    content = 'post_list category_id={category_id}, tag_id={tag_id}'.format(
+        category_id = category_id,
+        tag_id = tag_id,
+    )
+    return HttpResponse(content)
+
+def post_detail(request, post_id):
+    return render(request, 'blog/detail.html', context = {'name': 'post_detail'})
+    return HttpResponse('detail')
